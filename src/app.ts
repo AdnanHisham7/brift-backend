14199fb8b1b6c99fe899b1df55b444042bb068c7
AdnanHisham7 <a.hishamkk@gmail.com>
2026-02-25 21:31:58 +0530

    chore: setup database connection and global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { errorHandler } from "./interfaces/middleware/error-handler.middleware";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 app.get("/health", (_, res) => {
   res.status(200).json({ status: "OK" });
 });
+
+app.use(errorHandler);
 
 export default app;
